10e2e3773a54dd6adf5f2f5e6d8f841be937a59d
ganellope <gahyeonee00@gmail.com>
2019-06-26 23:46:29 +0900

    cloud firestore change

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/board1', require('./routes/board1'));
+//app.use('/board1', require('./routes/board1'));
+app.use('/board2', require('./routes/board2'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
